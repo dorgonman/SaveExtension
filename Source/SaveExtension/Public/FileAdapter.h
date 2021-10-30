12b110865098c56c61db5589d59dd17dc58e4d55
dorgon.chang <dorgonman@hotmail.com>
2021-10-30 22:45:56 +0800

    Fix UE5 Compile error

--- Source/SaveExtension/Public/FileAdapter.h
+++ Source/SaveExtension/Public/FileAdapter.h
@@ -62,7 +62,7 @@ struct FSaveFile
 {
 	int32 FileTypeTag = 0;
 	int32 SaveGameFileVersion = 0;
-	int32 PackageFileUEVersion = 0;
+	FPackageFileVersion PackageFileUEVersion;
 	FEngineVersion SavedEngineVersion;
 	int32 CustomVersionFormat = int32(ECustomVersionSerializationFormat::Unknown);
 	FCustomVersionContainer CustomVersions;
