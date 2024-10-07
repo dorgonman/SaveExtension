6cb5005891740b510addb52b7242c10ce750597b
git perforce import user <a@b>
2024-10-07 13:55:26 +0800

    Initial import of //UnrealEngine/Plugins/Marketplace/SaveExtension/ from the state at revision #head
    [git-p4: depot-paths = "//UnrealEngine/Plugins/Marketplace/SaveExtension/": change = 4740]

---
+++ Source/SaveExtension/SaveExtension.Build.cs
@@ -0,0 +1,28 @@
+// Copyright 2015-2020 Piperift. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.IO;
+
+namespace UnrealBuildTool.Rules {
+
+public class SaveExtension : ModuleRules
+{
+	public SaveExtension(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"Engine",
+			"Foliage",
+			"AIModule",
+			"CoreUObject",
+			"DeveloperSettings",
+			"ImageWrapper",
+			"NavigationSystem"
+		});
+	}
+}
+
+}
---
+++ Source/Test/SaveExtensionTest.Build.cs
@@ -0,0 +1,28 @@
+// Copyright 2015-2020 Piperift. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.IO;
+
+namespace UnrealBuildTool.Rules
+{
+	public class SaveExtensionTest : ModuleRules {
+		public SaveExtensionTest(ReadOnlyTargetRules Target) : base(Target)
+		{
+			PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+			PublicDependencyModuleNames.AddRange(new string[]
+			{
+				"Core",
+				"Engine",
+				"CoreUObject",
+				"SaveExtension",
+				"EngineSettings"
+			});
+
+            if (Target.bBuildEditor == true)
+            {
+                PrivateDependencyModuleNames.Add("UnrealEd");
+            }
+        }
+	}
+}
